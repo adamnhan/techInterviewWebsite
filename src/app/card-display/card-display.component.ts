8e019aa0f338449b54a5d7c22cd02ee75ef04576
Adam <atomnano47@gmail.com>
2024-03-01 13:58:35 -0500

    Actual final commit

--- src/app/card-display/card-display.component.ts
+++ src/app/card-display/card-display.component.ts
@@ -7,20 +7,20 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./card-display.component.scss']
 })
 export class CardDisplayComponent implements OnInit {
-  @Input() people: Person[] = []; // Assume data is passed as an input from the parent component
+  @Input() people: Person[] = []; 
 
   constructor() { }
 
   ngOnInit(): void {
   }
 }
-// card-display.component.ts or in a separate models file
+
 
 export interface Person {
   name: string;
   age: number;
   isStudent: boolean;
-  school?: string; // Optional since it's not required when the person is not a student
+  school?: string; 
   ageUnit: 'years' | 'months';
 }
 
